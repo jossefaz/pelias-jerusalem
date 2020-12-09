0ccad19e7c0fb1e4d162ac692a9c541d0fbf1e1c
jossefaz <jossefaz@protonmail.com>
2020-12-09 17:00:00 +0200

    pelias-start.sh

---
+++ pelias-start.sh
@@ -0,0 +1,11 @@
+!#/bin/zsh
+
+pelias compose pull
+pelias elastic start
+pelias elastic wait
+pelias elastic create
+pelias download all
+pelias prepare all
+pelias import all
+pelias import csv
+pelias compose up
